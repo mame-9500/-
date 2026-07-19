fa8cc7536f805a568307dc4d2723a1467b1abebb
mame-9500 <mame.ryusei619@gmail.com>
2026-07-19 18:13:17 +0900

    Add answer checking functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+function checkAnswer(correct, nextPage) {
+    const input = document.getElementById("answer");
+    const button = document.getElementById("next");
+
+    input.addEventListener("input", function () {
+        if (input.value.trim() === correct) {
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        }
+    });
+
+    button.addEventListener("click", function () {
+        window.location.href = nextPage;
+    });
+}
