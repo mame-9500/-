cbeb9ef19d31c80a641e7e0d9cd5d004486c8827
mame-9500 <mame.ryusei619@gmail.com>
2026-07-21 11:19:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-
 function checkAnswer(correct, nextPage) {
     const input = document.getElementById("answer");
     const button = document.getElementById("next");
 
+    // 入力内容を復元
+    const saved = sessionStorage.getItem(location.pathname);
+    if (saved) {
+        input.value = saved;
+        if (saved.trim() === correct) {
+            button.disabled = false;
+        }
+    }
+
     input.addEventListener("input", function () {
+        sessionStorage.setItem(location.pathname, input.value);
+
         if (input.value.trim() === correct) {
             button.disabled = false;
         } else {
